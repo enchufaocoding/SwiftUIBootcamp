18f1745631b4471d0261a63cd392cf4ade370a67
enchufaocoding <enchufaocoding@gmail.com>
2025-09-17 21:13:36 +0200

    TextBootcamp
    First Commit
    Create project

---
+++ TextBootcamp/TextBootcamp/TextBootcampApp.swift
@@ -0,0 +1,17 @@
+//
+//  TextBootcampApp.swift
+//  TextBootcamp
+//
+//  Created by Jose Alberto Rosario Castillo on 17/9/25.
+//
+
+import SwiftUI
+
+@main
+struct TextBootcampApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
