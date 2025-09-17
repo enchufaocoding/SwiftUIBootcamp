6ca79f3b7a067c2d0bcf2250793de8068562de67
enchufaocoding <enchufaocoding@gmail.com>
2025-09-17 22:29:31 +0200

    TextBootcamp - second update
    add modifiers to text
    add font
    add font to plist file
    use the custom font

--- TextBootcamp/TextBootcamp/ContentView.swift
+++ TextBootcamp/TextBootcamp/ContentView.swift
@@ -10,12 +10,23 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("iOS 26, iPadOS 26, macOS Tahoe 26, tvOS 26, visionOS 26, and watchOS 26 will soon be available to customers worldwide — which means you can now submit apps and games that take advantage of Apple’s broadest design update ever.")
+                .fontWeight(.bold)
+               // .font(.title)
+               // .font(.system(.title, design: .rounded))
+               // .font(.system(size: 20))
+                .font(.custom("Nunito-Regular", size: 20))
+                .foregroundStyle(.green)
+                .multilineTextAlignment(.center)
+               // .lineLimit(6)
+              //  .truncationMode(.head)
+                .lineSpacing(10)
+                .padding()
+                // .rotationEffect(Angle(degrees: 45))
+               // .rotationEffect(Angle(degrees: 20), anchor: UnitPoint(x: 0, y: 0)) // Anchor especifica el punto de rotacion.
+                .rotation3DEffect(Angle(degrees: 60), axis: (x: 1, y: 0, z: 0))
+                .shadow(color: .gray, radius: 2, x: 0, y: 15)
         }
-        .padding()
     }
 }
 
