15a8808485e866c87d7e41d7047c30889be7ca4c
enchufaocoding <enchufaocoding@gmail.com>
2025-09-17 22:33:41 +0200

    ImageBootcamp First Commit
    Create the project

---
+++ ImageBootcamp/ImageBootcamp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ImageBootcamp
+//
+//  Created by Jose Alberto Rosario Castillo on 17/9/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
