cb5ad0c4376709de67fca4603ef2717dd93aae92
EnchufaoCoding <61737343+enchufaocoding@users.noreply.github.com>
2025-10-15 19:57:44 +0200

    Add files via upload

---
+++ ListBootcamp/ListBootcamp/ContentView.swift
@@ -0,0 +1,125 @@
+//
+//  ContentView.swift
+//  ListBootcamp
+//
+//  Created by Jose Alberto Rosario Castillo on 27/9/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    var restaurants = [ Restaurant(name: "Cafe Deadend", image: "cafedeadend"),
+                   Restaurant(name: "Homei", image: "homei"),
+                   Restaurant(name: "Teakha", image: "teakha"),
+                   Restaurant(name: "Cafe Loisl", image: "cafeloisl"),
+                   Restaurant(name: "Petite Oyster", image: "petiteoyster"),
+                   Restaurant(name: "For Kee Restaurant", image: "forkeerestaurant"),
+                   Restaurant(name: "Po's Atelier", image: "posatelier"),
+                   Restaurant(name: "Bourke Street Bakery", image: "bourkestreetbakery"),
+                   Restaurant(name: "Haigh's Chocolate", image: "haighschocolate"),
+                   Restaurant(name: "Palomino Espresso", image: "palominoespresso"),
+                   Restaurant(name: "Homei", image: "upstate"),
+                   Restaurant(name: "Traif", image: "traif"),
+                   Restaurant(name: "Graham Avenue Meats And Deli", image: "grahamavenuemeats"),
+                   Restaurant(name: "Waffle & Wolf", image: "wafflewolf"),
+                   Restaurant(name: "Five Leaves", image: "fiveleaves"),
+                   Restaurant(name: "Cafe Lore", image: "cafelore"),
+                   Restaurant(name: "Confessional", image: "confessional"),
+                   Restaurant(name: "Barrafina", image: "barrafina"),
+                   Restaurant(name: "Donostia", image: "donostia"),
+                   Restaurant(name: "Royal Oak", image: "royaloak"),
+                   Restaurant(name: "CASK Pub and Kitchen", image: "caskpubkitchen")
+    ]
+    
+    var body: some View {
+        
+        List(restaurants) { restaurant in
+            // BasicImageRowView(restaurant: restaurant)
+            //   FullImageRow(restaurant: restaurant)
+            
+            ForEach(restaurants.indices, id: \.self) { index in
+                if (0...1).contains(index) {
+                    FullImageRow(restaurant: self.restaurants[index])
+                } else {
+                    BasicImageRowView(restaurant: self.restaurants[index])
+                }
+            }
+           // .listRowSeparatorTint(.red)
+           // .listRowSeparator(.hidden)
+        }
+       // .listStyle(.plain)
+        .background(
+            Image("barrafina")
+            .resizable()
+            .scaledToFill()
+            .clipped()
+            .ignoresSafeArea()
+        )
+        .scrollContentBackground(.hidden)
+        
+        /*
+        List (restaurantNames.indices, id: \.self) { index in
+            HStack {
+                Image(restaurantImages[index])
+                    .resizable()
+                    .frame(width: 40, height: 40)
+                    .cornerRadius(5)
+                Text(restaurantNames[index])
+                    .font(.headline)
+            }
+        }
+        .listStyle(.plain)
+        */
+    }
+}
+
+#Preview {
+    ContentView()
+}
+
+struct Restaurant: Identifiable {
+    let id = UUID()
+    var name: String
+    var image: String
+}
+
+struct BasicImageRowView: View {
+    var restaurant: Restaurant
+    
+    var body: some View {
+        HStack {
+            Image(restaurant.image)
+                .resizable()
+                .frame(width: 40, height: 40)
+                .cornerRadius(5)
+            Text(restaurant.name)
+                .font(.headline)
+        }
+    }
+}
+
+
+struct FullImageRow: View {
+    var restaurant: Restaurant
+    var body: some View {
+        ZStack {
+            Image(restaurant.image)
+                .resizable()
+                .aspectRatio(contentMode: .fill)
+                .frame(height: 200)
+                .cornerRadius(10)
+                .overlay(
+                    Rectangle()
+                        .foregroundStyle(.black)
+                        .cornerRadius(10)
+                        .opacity(0.2)
+                )
+            Text(restaurant.name)
+                .font(.system(.title, design: .rounded))
+                .fontWeight(.black)
+                .foregroundStyle(.white)
+        }
+    }
+}
+
---
+++ ListBootcamp/ListBootcamp/ListBootcampApp.swift
@@ -0,0 +1,17 @@
+//
+//  ListBootcampApp.swift
+//  ListBootcamp
+//
+//  Created by Jose Alberto Rosario Castillo on 27/9/25.
+//
+
+import SwiftUI
+
+@main
+struct ListBootcampApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
