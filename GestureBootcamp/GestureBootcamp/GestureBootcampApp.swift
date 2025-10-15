f75a1c4c440bb953f2e4930b12e003a3223b350f
EnchufaoCoding <61737343+enchufaocoding@users.noreply.github.com>
2025-10-15 19:56:15 +0200

    Add files via upload

---
+++ GestureBootcamp/GestureBootcamp/GestureBootcampApp.swift
@@ -0,0 +1,17 @@
+//
+//  GestureBootcampApp.swift
+//  GestureBootcamp
+//
+//  Created by Jose Alberto Rosario Castillo on 2/10/25.
+//
+
+import SwiftUI
+
+@main
+struct GestureBootcampApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
