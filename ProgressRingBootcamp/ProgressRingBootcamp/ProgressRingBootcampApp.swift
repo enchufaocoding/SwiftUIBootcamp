df30f1834a4c569700524f0ce6aeb8c990d8b443
EnchufaoCoding <61737343+enchufaocoding@users.noreply.github.com>
2025-10-15 20:05:41 +0200

    Add files via upload

---
+++ ProgressRingBootcamp/ProgressRingBootcamp/ProgressRingBootcampApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProgressRingBootcampApp.swift
+//  ProgressRingBootcamp
+//
+//  Created by Jose Alberto Rosario Castillo on 9/10/25.
+//
+
+import SwiftUI
+
+@main
+struct ProgressRingBootcampApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
