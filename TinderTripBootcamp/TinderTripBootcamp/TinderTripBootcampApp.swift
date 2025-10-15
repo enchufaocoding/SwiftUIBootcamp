cd147e0414bcfb2345282bb33b6bedf963c6538b
EnchufaoCoding <61737343+enchufaocoding@users.noreply.github.com>
2025-10-15 20:10:20 +0200

    Add files via upload

---
+++ TinderTripBootcamp/TinderTripBootcamp/TinderTripBootcampApp.swift
@@ -0,0 +1,17 @@
+//
+//  TinderTripBootcampApp.swift
+//  TinderTripBootcamp
+//
+//  Created by Jose Alberto Rosario Castillo on 3/10/25.
+//
+
+import SwiftUI
+
+@main
+struct TinderTripBootcampApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
