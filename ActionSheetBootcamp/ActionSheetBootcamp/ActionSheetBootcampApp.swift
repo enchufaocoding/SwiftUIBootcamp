50811c1f5d1bb0f0f60890937c21aaef00910a5c
EnchufaoCoding <61737343+enchufaocoding@users.noreply.github.com>
2025-10-15 19:51:40 +0200

    Add files via upload

---
+++ ActionSheetBootcamp/ActionSheetBootcamp/ActionSheetBootcampApp.swift
@@ -0,0 +1,17 @@
+//
+//  ActionSheetBootcampApp.swift
+//  ActionSheetBootcamp
+//
+//  Created by Jose Alberto Rosario Castillo on 1/10/25.
+//
+
+import SwiftUI
+
+@main
+struct ActionSheetBootcampApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ActionSheetBootcamp/ActionSheetBootcamp/ContentView.swift
@@ -0,0 +1,194 @@
+//
+//  ContentView.swift
+//  ActionSheetBootcamp
+//
+//  Created by Jose Alberto Rosario Castillo on 1/10/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    @State var restaurants = [ Restaurant(name: "Cafe Deadend", image: "cafedeadend"),
+                               Restaurant(name: "Homei", image: "homei"),
+                               Restaurant(name: "Teakha", image: "teakha"),
+                               Restaurant(name: "Cafe Loisl", image: "cafeloisl"),
+                               Restaurant(name: "Petite Oyster", image: "petiteoyster"),
+                               Restaurant(name: "For Kee Restaurant", image: "forkeerestaurant"),
+                               Restaurant(name: "Po's Atelier", image: "posatelier"),
+                               Restaurant(name: "Bourke Street Bakery", image: "bourkestreetbakery"),
+                               Restaurant(name: "Haigh's Chocolate", image: "haighschocolate"),
+                               Restaurant(name: "Palomino Espresso", image: "palominoespresso"),
+                               Restaurant(name: "Homei", image: "upstate"),
+                               Restaurant(name: "Traif", image: "traif"),
+                               Restaurant(name: "Graham Avenue Meats And Deli", image: "grahamavenuemeats"),
+                               Restaurant(name: "Waffle & Wolf", image: "wafflewolf"),
+                               Restaurant(name: "Five Leaves", image: "fiveleaves"),
+                               Restaurant(name: "Cafe Lore", image: "cafelore"),
+                               Restaurant(name: "Confessional", image: "confessional"),
+                               Restaurant(name: "Barrafina", image: "barrafina"),
+                               Restaurant(name: "Donostia", image: "donostia"),
+                               Restaurant(name: "Royal Oak", image: "royaloak"),
+                               Restaurant(name: "CASK Pub and Kitchen", image: "caskpubkitchen")
+    ]
+    
+    @State private var showActionSheet = false
+    @State private var selectedRestaurant: Restaurant?
+    
+    var body: some View {
+        
+        List {
+            ForEach(restaurants) { restaurant in
+                BasicImageRow(restaurant: restaurant)
+                
+                    .contextMenu {
+                        
+                        Button {
+                            // delete
+                            self.setCheckIn(item : restaurant)
+                        } label: {
+                            HStack {
+                                Text("Check-In")
+                                Image(systemName: "checkmark.seal.fill")
+                            }
+                            
+                        }
+                        
+                        Button {
+                            // delete
+                            self.delete(item : restaurant)
+                        } label: {
+                            HStack {
+                                Text("Delete")
+                                Image(systemName: "trash")
+                            }
+                            
+                        }
+                        
+                        
+                        Button {
+                            //favorite
+                            setFavorite(item: restaurant)
+                        } label: {
+                            HStack {
+                                Text("Favorite")
+                                Image(systemName: "star")
+                            }
+                        }
+                        
+                    }
+                
+                    .onTapGesture {
+                        self.showActionSheet.toggle()
+                        self.selectedRestaurant = restaurant
+                    }
+                
+                    .actionSheet(item: self.$selectedRestaurant) { restaurant in
+                        ActionSheet(title: Text("What do you want to do"), message: nil, buttons: [
+                            .default(Text("Mark as Favorite"), action: {
+                                self.setFavorite(item: restaurant)
+                            }),
+                            .destructive(Text("Delete"), action: {
+                                self.delete(item: restaurant)
+                            }),
+                            .cancel()
+                        ])
+                    }
+                /*
+                    .actionSheet(isPresented: self.$showActionSheet) {
+                        ActionSheet(title: Text("What do you want to do"), message: nil, buttons: [
+                            .default(Text("Mark as Favorite"), action: {
+                                if let selectedRestaurant = self.selectedRestaurant {
+                                    self.setFavorite(item: selectedRestaurant)
+                                }
+                            }),
+                            .destructive(Text("Delete"), action: {
+                                if let selectedRestaurant = self.selectedRestaurant {
+                                    self.delete(item: selectedRestaurant)
+                                }
+                            }),
+                            .cancel()
+                        ])
+                    }
+                */
+            }
+            .onDelete { (indexSet) in
+                self.restaurants.remove(atOffsets: indexSet)
+            }
+            
+        }
+        .listStyle(.plain)
+        
+    }
+    
+    // Funtions: Delete and Favorites.-
+    // Delete
+    private func delete(item restaurant: Restaurant) {
+        if let index = self.restaurants.firstIndex(where: {$0.id == restaurant.id})
+        {
+            self.restaurants.remove(at: index)
+        }
+    }
+    
+    // add to Favorites
+    private func setFavorite(item restaurant: Restaurant) {
+        if let index = self.restaurants.firstIndex(where: {$0.id == restaurant.id})
+        {
+            self.restaurants[index].isFavorite.toggle()
+        }
+    }
+    
+    // add to CheckInIcon
+    private func setCheckIn(item restaurant: Restaurant) {
+        if let index = self.restaurants.firstIndex(where: {$0.id == restaurant.id})
+        {
+            self.restaurants[index].isCheckIn.toggle()
+        }
+    }
+    
+    
+    
+}
+
+#Preview {
+    ContentView()
+}
+
+struct Restaurant: Identifiable {
+    var id = UUID()
+    var name: String
+    var image: String
+    var isFavorite: Bool = false
+    var isCheckIn: Bool = false
+}
+
+struct BasicImageRow: View {
+    var restaurant: Restaurant
+    
+    var body: some View {
+        HStack {
+            Image(restaurant.image)
+                .resizable()
+                .frame(width: 40, height: 40)
+                .cornerRadius(5)
+            Text(restaurant.name)
+        
+                if restaurant.isCheckIn {
+               //     Spacer()
+                    
+                    Image(systemName: "checkmark.seal.fill")
+                        .foregroundColor(.red)
+                }
+            
+            HStack{
+                if restaurant.isFavorite {
+                    Spacer()
+                    
+                    Image(systemName: "star.fill")
+                        .foregroundColor(.yellow)
+                }
+            }
+            
+        }
+    }
+}
