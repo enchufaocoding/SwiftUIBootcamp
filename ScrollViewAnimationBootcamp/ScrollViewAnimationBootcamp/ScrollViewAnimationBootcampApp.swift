10dcf69e689d8cd9994c9c9f0cf8c07c39b38924
EnchufaoCoding <61737343+enchufaocoding@users.noreply.github.com>
2025-10-16 23:27:39 +0200

    Add files via upload

---
+++ ScrollViewAnimationBootcamp/ScrollViewAnimationBootcamp/ScrollViewAnimationBootcampApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrollViewAnimationBootcampApp.swift
+//  ScrollViewAnimationBootcamp
+//
+//  Created by Jose Alberto Rosario Castillo on 16/10/25.
+//
+
+import SwiftUI
+
+@main
+struct ScrollViewAnimationBootcampApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
