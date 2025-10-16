4cd7fdfbb1d50cafa095ebdf393ce897fc43404a
EnchufaoCoding <61737343+enchufaocoding@users.noreply.github.com>
2025-10-16 23:27:07 +0200

    Add files via upload

---
+++ PieDonutChartBootcamp/PieDonutChartBootcamp/PieDonutChartBootcampApp.swift
@@ -0,0 +1,17 @@
+//
+//  PieDonutChartBootcampApp.swift
+//  PieDonutChartBootcamp
+//
+//  Created by Jose Alberto Rosario Castillo on 16/10/25.
+//
+
+import SwiftUI
+
+@main
+struct PieDonutChartBootcampApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
