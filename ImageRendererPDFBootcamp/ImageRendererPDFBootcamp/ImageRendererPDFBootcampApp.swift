b0ee7f80e45f2e11888d36c9eea5295dc3cbb27f
EnchufaoCoding <61737343+enchufaocoding@users.noreply.github.com>
2025-10-16 23:26:36 +0200

    Add files via upload

---
+++ ImageRendererPDFBootcamp/ImageRendererPDFBootcamp/ImageRendererPDFBootcampApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImageRendererPDFBootcampApp.swift
+//  ImageRendererPDFBootcamp
+//
+//  Created by Jose Alberto Rosario Castillo on 16/10/25.
+//
+
+import SwiftUI
+
+@main
+struct ImageRendererPDFBootcampApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
