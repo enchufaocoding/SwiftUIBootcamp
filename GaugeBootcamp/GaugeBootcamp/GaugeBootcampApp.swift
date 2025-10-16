cd1e03616f43dbf32fb42bab8ba9a531bd5724fb
EnchufaoCoding <61737343+enchufaocoding@users.noreply.github.com>
2025-10-16 23:25:43 +0200

    Add files via upload

---
+++ GaugeBootcamp/GaugeBootcamp/GaugeBootcampApp.swift
@@ -0,0 +1,17 @@
+//
+//  GaugeBootcampApp.swift
+//  GaugeBootcamp
+//
+//  Created by Jose Alberto Rosario Castillo on 16/10/25.
+//
+
+import SwiftUI
+
+@main
+struct GaugeBootcampApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
