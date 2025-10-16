85ca7bcd398b423b53c417a64c50f149b5369347
EnchufaoCoding <61737343+enchufaocoding@users.noreply.github.com>
2025-10-16 23:26:15 +0200

    Add files via upload

---
+++ ImageRendererBootcamp/ImageRendererBootcamp/ImageRendererBootcampApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImageRendererBootcampApp.swift
+//  ImageRendererBootcamp
+//
+//  Created by Jose Alberto Rosario Castillo on 16/10/25.
+//
+
+import SwiftUI
+
+@main
+struct ImageRendererBootcampApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
