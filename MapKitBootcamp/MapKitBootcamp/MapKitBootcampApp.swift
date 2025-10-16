2647a5a88c39a565a4d8a6a910743f6128a4e764
EnchufaoCoding <61737343+enchufaocoding@users.noreply.github.com>
2025-10-16 23:26:52 +0200

    Add files via upload

---
+++ MapKitBootcamp/MapKitBootcamp/MapKitBootcampApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapKitBootcampApp.swift
+//  MapKitBootcamp
+//
+//  Created by Jose Alberto Rosario Castillo on 16/10/25.
+//
+
+import SwiftUI
+
+@main
+struct MapKitBootcampApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
