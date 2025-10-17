383c6e91175c779d75ebe341b0363514d34bfa22
EnchufaoCoding <61737343+enchufaocoding@users.noreply.github.com>
2025-10-17 23:11:58 +0200

    Add files via upload

---
+++ PhotosPickerBootcamp/PhotosPickerBootcamp/PhotosPickerBootcampApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhotosPickerBootcampApp.swift
+//  PhotosPickerBootcamp
+//
+//  Created by Jose Alberto Rosario Castillo on 17/10/25.
+//
+
+import SwiftUI
+
+@main
+struct PhotosPickerBootcampApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
