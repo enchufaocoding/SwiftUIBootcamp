7766f0a71ad27f2563c14eafe6d08646e2c96bf8
EnchufaoCoding <61737343+enchufaocoding@users.noreply.github.com>
2025-10-17 23:12:34 +0200

    Add files via upload

---
+++ KeyframeAnimatorBootcamp/KeyframeAnimatorBootcamp/KeyframeAnimatorBootcampApp.swift
@@ -0,0 +1,17 @@
+//
+//  KeyframeAnimatorBootcampApp.swift
+//  KeyframeAnimatorBootcamp
+//
+//  Created by Jose Alberto Rosario Castillo on 17/10/25.
+//
+
+import SwiftUI
+
+@main
+struct KeyframeAnimatorBootcampApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
