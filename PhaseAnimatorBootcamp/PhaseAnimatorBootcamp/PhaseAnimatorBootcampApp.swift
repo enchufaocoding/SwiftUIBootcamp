674392607179ce17ff2c2d0876944cd5bbf01c33
EnchufaoCoding <61737343+enchufaocoding@users.noreply.github.com>
2025-10-17 23:12:14 +0200

    Add files via upload

---
+++ PhaseAnimatorBootcamp/PhaseAnimatorBootcamp/PhaseAnimatorBootcampApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhaseAnimatorBootcampApp.swift
+//  PhaseAnimatorBootcamp
+//
+//  Created by Jose Alberto Rosario Castillo on 17/10/25.
+//
+
+import SwiftUI
+
+@main
+struct PhaseAnimatorBootcampApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
