51067ce8cb8afb58b7ea3b1edde64528c93e23d4
EnchufaoCoding <61737343+enchufaocoding@users.noreply.github.com>
2025-10-18 09:27:03 +0200

    Add files via upload

---
+++ MatchedGeometryBootcamp/MatchedGeometryBootcamp/MatchedGeometryBootcampApp.swift
@@ -0,0 +1,17 @@
+//
+//  MatchedGeometryBootcampApp.swift
+//  MatchedGeometryBootcamp
+//
+//  Created by Jose Alberto Rosario Castillo on 18/10/25.
+//
+
+import SwiftUI
+
+@main
+struct MatchedGeometryBootcampApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
